ac0858b1d829f8c1d28bf9f37880488599342816
amit-saini <amit.saini@innostax.com>
2016-12-16 16:03:05 +0530

    configuration changes

---
+++ ChartMogul/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartMogul.API
+{
+    class Configuration
+    {
+        public static string AccountToken
+        {
+            get { return ConfigurationManager.AppSettings["AccountToken"]; }
+        }
+        public static string BaseUrl
+        {
+            get { return ConfigurationManager.AppSettings["BaseUrl"]; }
+        }
+        public static string SecretKey
+        {
+            get { return ConfigurationManager.AppSettings["SecretKey"]; }
+        }
+    }
+}
---
+++ ChartMogul/Enums/RequestMethod.cs
@@ -0,0 +1,10 @@
+﻿namespace ChartMogul.API.Enums
+{
+    enum RequestMethod
+    {
+        Delete,
+        Get,
+        Post,
+        Put
+    }
+}
