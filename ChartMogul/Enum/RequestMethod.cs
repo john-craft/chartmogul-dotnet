55bc86d0c9f80d49719633b607f161172efc16a7
sehraashish <ashish.sehra@vicesoftware.com>
2016-12-16 13:58:16 +0530

    Added new HTTP

---
+++ ChartMogul/Enum/RequestMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartMogul.API.Enum
+{
+    public enum RequestMethod
+    {
+        Delete,
+        Get,
+        Post,
+        Put
+    }
+}
